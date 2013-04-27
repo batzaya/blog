527594642a404eabe7c3a0a5a35703fe73c9ff4d
muugii <muugii@muugii-desktop.(none)>
2013-04-27 11:12:33 +0800

    I1: Өгөгдлийн сангаас хэрэглэгчийн мэдээллийг шалгаж оруулдаг болгосон.

---
+++ session.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+$connect = mysqli_connect("localhost","root","","blog");
+//$query = "SELECT * FROM admin";
+$result = mysqli_query($connect,"SELECT * FROM admin");
+if (isset($_GET['logout'])){
+    unset($_SESSION['logged_in']);
+    $message = "ta garsan bna";
+}
+if (isset($_POST['login'])){
+    while ($row = mysqli_fetch_assoc($result)){
+        if($row['name'] == $_POST['username'] && $row['password'] == $_POST['password']){
+            $_SESSION['logged_in'] = 'true';
+        }
+    }
+    if (!isset($_SESSION['logged_in'])){
+        $message = "ta buru password oruulsan bna ";
+    }
+}
+if (isset($_SESSION['logged_in'])){
+    $message = "ta nevtersen bna";
+}
+
+$logged_in = isset($_SESSION['logged_in']);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+    <?php if ($logged_in){ ?>
+        <a href="session.php?logout=0">log out</a>
+    <?php }else { ?>
+        <a href="login.php"> log in</a>    
+    <?php } ?>
+
+    <hr />
+    
+    <p><?php echo $message ?></p>
+</body>
+</html>
+
